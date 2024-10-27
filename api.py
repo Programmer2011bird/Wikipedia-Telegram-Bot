6d75ac28a7ec094e5d190823d6ef15b2ed8c8c9e
Programmer2011bird <maysamshaker7@gmail.com>
2024-10-27 19:37:12 +0330

    getting full html

--- api.py
+++ api.py
@@ -2,32 +2,39 @@ import requests
 
 
 class Downloader:
-    def __init__(self, URL: str, filePath: str):
+    def __init__(self, Content: str, filePath: str):
         pass
 
 class API_HANDLER:
     def __init__(self) -> None:
         self.URL: str = "https://en.wikipedia.org/api/rest_v1"
 
-    def getFullHTML(self, title: str):
+    def getFullHTML(self, title: str) -> str:
         self.ENDPOINT: str = f"{self.URL}/page/html/{title}"
         
         RESPONSE: requests.Response = requests.get(self.ENDPOINT)
-        RESPONSE_JSON: dict = dict(RESPONSE.json())
+        RESPONSE_CONTENT: str = RESPONSE.content.decode()
+
+        return RESPONSE_CONTENT
 
     def getFullPDF(self, title: str):
         self.ENDPOINT: str = f"{self.URL}/page/pdf/{title}/a4"
 
         RESPONSE: requests.Response = requests.get(self.ENDPOINT)
-        RESPONSE_JSON: dict = dict(RESPONSE.json())
+        RESPONSE_JSON = RESPONSE.json()
+
+        # print(RESPONSE_JSON)
 
     def getSummary(self, title: str):
         self.ENDPOINT: str = f"{self.URL}/page/summary/{title}"
 
         RESPONSE: requests.Response = requests.get(self.ENDPOINT)
-        RESPONSE_JSON: dict = dict(RESPONSE.json())
+        RESPONSE_JSON = RESPONSE.json()
+
+        # print(RESPONSE_JSON)
 
 
 if __name__ == "__main__":
     API: API_HANDLER = API_HANDLER()
+    print(API.getFullHTML("Earth"))
 
