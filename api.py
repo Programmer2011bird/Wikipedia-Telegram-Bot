1dedabad1e9cad62cb82e299cd09cad30490b37c
Programmer2011bird <maysamshaker7@gmail.com>
2024-10-27 19:29:37 +0330

    More API code boilerplate

--- api.py
+++ api.py
@@ -7,16 +7,25 @@ class Downloader:
 
 class API_HANDLER:
     def __init__(self) -> None:
-        self.URL: str = "https://en.wikipedia.org/api/rest_v1/"
+        self.URL: str = "https://en.wikipedia.org/api/rest_v1"
 
     def getFullHTML(self, title: str):
-        self.ENDPOINT: str = ""
+        self.ENDPOINT: str = f"{self.URL}/page/html/{title}"
+        
+        RESPONSE: requests.Response = requests.get(self.ENDPOINT)
+        RESPONSE_JSON: dict = dict(RESPONSE.json())
 
     def getFullPDF(self, title: str):
-        self.ENDPOINT: str = ""
+        self.ENDPOINT: str = f"{self.URL}/page/pdf/{title}/a4"
+
+        RESPONSE: requests.Response = requests.get(self.ENDPOINT)
+        RESPONSE_JSON: dict = dict(RESPONSE.json())
 
     def getSummary(self, title: str):
-        self.ENDPOINT: str = ""
+        self.ENDPOINT: str = f"{self.URL}/page/summary/{title}"
+
+        RESPONSE: requests.Response = requests.get(self.ENDPOINT)
+        RESPONSE_JSON: dict = dict(RESPONSE.json())
 
 
 if __name__ == "__main__":
